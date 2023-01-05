4c27691ea443057b5b4a4b22c6f480f82709cff1
kimgenie0409 <kimgenie0409@gmail.com>
2023-01-05 21:01:58 +0900

    [Update] First Flexible box

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,31 +11,30 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
       body: Column(
         children: [
           Flexible(
             flex: 1,
             child: Container(
-              decoration: const BoxDecoration(
-                color: Colors.red,
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                '25:00',
+                style: TextStyle(
+                  color: Theme.of(context).cardColor,
+                  fontSize: 89,
+                  fontWeight: FontWeight.w600,
+                ),
               ),
             ),
           ),
           Flexible(
             flex: 2,
-            child: Container(
-              decoration: const BoxDecoration(
-                color: Colors.green,
-              ),
-            ),
+            child: Container(),
           ),
           Flexible(
             flex: 1,
-            child: Container(
-              decoration: const BoxDecoration(
-                color: Colors.blue,
-              ),
-            ),
+            child: Container(),
           ),
         ],
       ),
